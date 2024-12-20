72530149c03c4e11bde1d7ee23712bbad5633618
abiel almonte <aalmo061@fiu.edu>
2024-12-20 14:00:44 -0500

    style: rename variables for clarity

--- utils/populate_df.py
+++ utils/populate_df.py
@@ -21,8 +21,10 @@ if __name__ == '__main__':
     master_df= pd.merge(master_df, input_images_df, how= 'left', left_on= 'lkey_input', right_on= 'rkey_input')
     
     output_text_files= glob.glob("./data/image_name_output_*.txt")
-    all_output_df = pd.DataFrame()
+    all_output_images_df = pd.DataFrame()
+    
     for file in output_text_files:
+        
         file_name= os.path.basename(file)
         column_suffix= file_name.replace("image_name_output_", "").replace(".txt", "")
         column_name= f"output_image_path_{column_suffix}"
@@ -31,12 +33,12 @@ if __name__ == '__main__':
         
         df['rkey']= df[column_name].apply(get_key_name)
         
-        if all_output_df.empty:
-            all_output_df= df
+        if all_output_images_df.empty:
+            all_output_images_df= df
         else:
-            all_output_df= pd.merge(all_output_df, df, on='rkey', how='outer')
+            all_output_images_df= pd.merge(all_output_images_df, df, on='rkey', how='outer')
     
-    final_df= pd.merge(master_df, all_output_df, how='left', left_on='lkey', right_on='rkey')
+    final_df= pd.merge(master_df, all_output_images_df, how='left', left_on='lkey', right_on='rkey')
     final_df.drop(columns= ['lkey', 'rkey', 'lkey_input', 'rkey_input'], inplace= True)
     
     final_df.to_csv('./data/Prompts_Final_Categories_with_Image_Paths.csv', index= False)
